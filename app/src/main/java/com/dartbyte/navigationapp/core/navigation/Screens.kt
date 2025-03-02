966907fc5cef732ca74f3c7b77007e2837ff6c3a
jrvdev <188245562+jrvdev@users.noreply.github.com>
2025-03-02 12:06:08 -0400

    feat: agregando el componente de pantallas

---
+++ app/src/main/java/com/dartbyte/navigationapp/core/navigation/Screens.kt
@@ -0,0 +1,12 @@
+package com.dartbyte.navigationapp.core.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Login
+
+@Serializable
+object Home
+
+@Serializable
+data class Detail(val name:String)
